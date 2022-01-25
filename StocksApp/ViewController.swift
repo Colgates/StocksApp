804c30ce728d66b847272d34c0864581afda4c05
Evgenii Kolgin <mista.colgate@gmail.com>
2022-01-25 21:45:10 +0800

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Evgenii Kolgin on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
